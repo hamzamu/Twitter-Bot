f0d90882fad3f2b8191b5c1f1db75535ad234651
Jagan Ganesh <jaganganesh@outlook.com>
2019-01-24 12:38:39 +0530

    Initial Release

--- bot.js
+++ bot.js
@@ -1,1 +1,42 @@
-
+// Packages
+var twit = require('twit');
+// Configuration
+var config = require('./config.js');
+var Twitter = new twit(config);
+// Retweet
+var retweet = function() {
+    // Parameters
+    var params = {
+        q: '#github, #GitHub',
+        result_type: 'recent',
+        lang: 'en'
+    }
+    // Search
+    Twitter.get('search/tweets', params, function(err, data) {
+        // Success
+        if (!err) {
+            // Grab Tweet ID
+            var retweetId = data.statuses[0].id_str;
+            // Retweet
+            Twitter.post('statuses/retweet/:id', {
+                id: retweetId
+            }, function(err, response) {
+                if (response) {
+                    console.log('Retweeted : ', retweetId);
+                }
+                if (err) {
+                    console.log('Duplicate : ', retweetId);
+                }
+                console.log('');
+            });
+        } else {
+            console.log('Search Error');
+        }
+    });
+}
+// Date
+const now = new Date();
+console.log('\n', now ,'\n');
+// Retweet Function
+retweet();
+setInterval(retweet, 60000);
