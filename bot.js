b659dd98f25e2386f18e06f68275683393407278
Jagan Ganesh <JaganGanesh@outlook.com>
2019-02-05 18:40:55 +0000

    Favorite Function - Added

--- bot.js
+++ bot.js
@@ -34,9 +34,44 @@ var retweet = function() {
         }
     });
 }
+// Favorite
+var favoriteTweet = function(){
+	// Parameters
+	var params = {
+		q: '#github, #GitHub',
+		result_type: 'recent',
+		lang: 'en'
+	}
+	// Grab Twitter ID
+	Twitter.get('search/tweets', params, function(err,data){
+		var tweet = data.statuses;
+		var randomTweet = ranDom(tweet);
+		// If Exists
+		if(typeof randomTweet != 'undefined'){
+			// Favorite
+			Twitter.post('favorites/create', {id: randomTweet.id_str}, function(err, response){
+				// Success
+				if(err){
+					console.log('Favorite Error');
+				} else {
+					console.log('Favorite Success');
+				}
+				console.log('');
+			});
+		}
+	});
+}
 // Date
 const now = new Date();
 console.log('\n', now ,'\n');
 // Retweet Function
 retweet();
 setInterval(retweet, 60000);
+// Favorite Function
+favoriteTweet();
+setInterval(favoriteTweet, 600000);
+// Random Tweet
+function ranDom (arr) {
+	var index = Math.floor(Math.random()*arr.length);
+	return arr[index];
+};
